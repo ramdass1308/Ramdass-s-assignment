e67edafd34a0be69100599a1c5f28382d22c5320
ramdass1308 <90891927+ramdass1308@users.noreply.github.com>
2021-09-17 12:08:41 +0530

    Day 1 Assignments submitted

---
+++ Day 1/Day1float.py
@@ -0,0 +1,4 @@
+int_number = 1383
+
+float_number = float(int_number)
+print(float_number)
---
+++ Day 1/day1string.py
@@ -0,0 +1,14 @@
+my_string = "Hello"
+print (my_string)
+
+my_string = "To The"
+print (my_string)
+
+my_string = "World of"
+print (my_string)
+
+my_string = "Python"
+print (my_string)
+
+my_string = "LETS UPGRADE"
+print (my_string)
